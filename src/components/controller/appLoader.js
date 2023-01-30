b977100817d8a09e2526c8867b641574f7fb7075
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-01-30 15:46:44 +0100

    fix key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,9 @@ class AppLoader extends Loader {
     apiKey;
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'dd7ccd0a32494fb691c471a79c824e0c', // получите свой ключ https://newsapi.org/
+            apiKey: '189b5038bfe94d7fa5c972b24790ee64', // получите свой ключ https://newsapi.org/
+
+            // apiKey: 'dd7ccd0a32494fb691c471a79c824e0c', // получите свой ключ https://newsapi.org/
         });
     }
 }
